33314d98ecb40b53ef0f37c1a34f660b29cbe981
NAWARXVOND <nhxg369@gmail.com>
2026-08-25 21:06:27 +0400

    Merge Alembic heads after action request migration

---
+++ migrations/versions/d9f4c2a1b760_merge_action_and_analytics_heads.py
@@ -0,0 +1,18 @@
+"""merge action request and analytics migration heads
+
+Revision ID: d9f4c2a1b760
+Revises: ab41d9f2c7e6, b73e8f10c5d2
+"""
+
+revision = "d9f4c2a1b760"
+down_revision = ("ab41d9f2c7e6", "b73e8f10c5d2")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
